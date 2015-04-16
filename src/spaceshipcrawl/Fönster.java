a61e367bba86a869f1fb09592623f505691918c1
Samai <Samai96@live.se>
2015-04-16 09:03:21 +0200

    Runloop

--- src/spaceshipcrawl/Fönster.java
+++ src/spaceshipcrawl/Fönster.java
@@ -8,6 +8,8 @@ import java.awt.*;
  * @author petmeu239
  */
 public class Fönster extends Applet implements Runnable {
+    private boolean running = true;
+    private boolean paused = false;
     
         @Override
         public void init(){
@@ -17,13 +19,47 @@ public class Fönster extends Applet implements Runnable {
         public void paint(Graphics g){
             
         }
-        public void update(){
+        public void update(double delta){
             
         }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        long lastLoopTime = System.nanoTime();
+        final int TARGET_FPS = 60;
+        final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+        int fps = 0;
+        int lastFpsTime = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            long updateLength = now - lastLoopTime;
+            lastLoopTime = now;
+            double delta = updateLength / ((double) OPTIMAL_TIME);
+            lastFpsTime += updateLength;
+            fps++;
+            if (lastFpsTime >= 1000000000) {
+                //System.out.println("(FPS: " + fps + ")");
+                System.out.println("(FPS: " + fps + ")");
+                lastFpsTime = 0;
+                fps = 0;
+            }
+            if (!paused) {
+                update(delta);
+            }
+
+            repaint();
+
+            long l = (lastLoopTime  + OPTIMAL_TIME);
+            if (l < 0) {
+
+            } else {
+                while (System.nanoTime()-l<0 ) {
+                    Thread.yield();
+                }
+
+            }
+        }
     }
                 
     
