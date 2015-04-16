25d03c00ee2f9567b1e90c232490621ebc93995c
soridanm <petmeu239@E29623.edu.linkoping.se>
2015-04-16 09:00:44 +0200

    Försa fönster

---
+++ src/spaceshipcrawl/Fönster.java
@@ -0,0 +1,31 @@
+package spaceshipcrawl;
+
+import java.applet.*;
+import java.awt.*;
+
+/**
+ *
+ * @author petmeu239
+ */
+public class Fönster extends Applet implements Runnable {
+    
+        @Override
+        public void init(){
+            
+        }
+        @Override
+        public void paint(Graphics g){
+            
+        }
+        public void update(){
+            
+        }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+                
+    
+    
+}
