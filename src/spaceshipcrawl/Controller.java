3d22c614e9ca94c9e2345932258a97bf8665a5d0
Samai <simon.lindgren2@gmail.com>
2015-04-17 20:13:21 +0200

    Revert "Delete"
    This reverts commit 454b1f21145b77d7e89f1a838d908c38cae1ae63.

---
+++ src/spaceshipcrawl/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spaceshipcrawl;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author simlin329
+ */
+public class Controller {
+
+    void paint(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void update(double delta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
