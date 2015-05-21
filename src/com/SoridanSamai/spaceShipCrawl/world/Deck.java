eb54a4803245c63382ad4e77408f6430d00e59c7
Samai <Samai96@live.se>
2015-05-21 08:44:41 +0200

    Weighted points

--- src/com/SoridanSamai/spaceShipCrawl/world/Deck.java
+++ src/com/SoridanSamai/spaceShipCrawl/world/Deck.java
@@ -3,6 +3,7 @@ package com.SoridanSamai.spaceShipCrawl.world;
 import com.SoridanSamai.spaceShipCrawl.Reference;
 import com.SoridanSamai.spaceShipCrawl.enemies.Entity;
 import com.SoridanSamai.spaceShipCrawl.enemies.Player;
+import com.SoridanSamai.spaceShipCrawl.other.WeightedPoint;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -101,9 +102,9 @@ public class Deck {
 
             if (e1 instanceof Player) {
 //                ta position
-                Point F = new Point(e1.getPosition());
+                WeightedPoint F = new WeightedPoint(e1.getPosition());
                 set.add(F);
-                floodfill(set);
+                floodfill(set,0);
                 visited.clear();
             }
             collissions(e1, delta);
@@ -118,12 +119,12 @@ public class Deck {
             e1.updatelastpos();
         }
     }
-    HashSet<Point> set = new HashSet();
-    HashSet<Point> visited = new HashSet();
+    HashSet<WeightedPoint> set = new HashSet();
+    HashSet<WeightedPoint> visited = new HashSet();
 
-    private void floodfill(HashSet<Point> set) {
-        HashSet<Point> nextSet = new HashSet();
-        for (Point p : set) {
+    private void floodfill(HashSet<WeightedPoint> set,int weight) {
+        HashSet<WeightedPoint> nextSet = new HashSet();
+        for (WeightedPoint p : set) {
             if (set.isEmpty() || !isPassable(p.x, p.y)) {
             } else {
 
@@ -132,10 +133,10 @@ public class Deck {
                         if (i == 0 && j == 0) {
                             continue;
                         }
-                        if (isPassable(p.x + i, p.y + j) && !visited.contains(new Point(p.x + i, p.y + j))) {
-                            nextSet.add(new Point(p.x + i, p.y + j));
+                        if (isPassable(p.x + i, p.y + j) && !visited.contains(new WeightedPoint(p.x + i, p.y + j))) {
+                            nextSet.add(new WeightedPoint(p.x + i, p.y + j));
                             for (Entity e : population) {
-                                if (e.getPosition().equals(new Point(p.x + i, p.y + j))) {
+                                if (e.getPosition().equals(new WeightedPoint(p.x + i, p.y + j))) {
                                     e.setTarget(p);
                                 }
                             }
@@ -144,7 +145,7 @@ public class Deck {
                 }
 
                 visited.add(p);
-                floodfill(nextSet);
+                floodfill(nextSet,weight);
                 nextSet.clear();
             }
         }
